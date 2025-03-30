b20078183a03faa3406692d49f781871a619dc06
Michael10504 <michaelredaa.2005@gmail.com>
2025-03-30 17:10:51 +0200

    Added calculator.cpp and calculator.h

---
+++ calculator.cpp
@@ -0,0 +1,45 @@
+#include "calculator.h"
+#include <ctime>
+#include <iostream>
+using namespace std;
+Calculator::Calculator(int x, int y) : x(x), y(y) {}
+int Calculator::addNums(int x, int y)
+{
+    return x + y;
+}
+int Calculator::subtractNums(int x, int y)
+{
+    return x - y;
+}
+int Calculator::multiplyNums(int x, int y)
+{
+    return x * y;
+}
+float Calculator::divideNums(float x, float y)
+{
+    return x / y;
+}
+int Calculator::gcd(int x, int y)
+{
+    int z = (x < y) ? x : y;
+    while (z > 0)
+    {
+        if (x % z == 0 && y % z == 0)
+            return z;
+        z--;
+    }
+    return 1;
+}
+int Calculator::lcm(int x, int y)
+{
+}
+int Calculator::factorial(int x)
+{
+    if (x == 1 || x == 0)
+        return 1;
+    else
+        return x * factorial(x - 1);
+}
+int Calculator::random(int rangelim1, int rangelim2)
+{
+}
